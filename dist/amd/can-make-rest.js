7d58886324eb7b84fac8eca67a9448cff6793176
Justin Meyer <justinbmeyer@gmail.com>
2017-06-02 10:19:28 -0500

    0.0.2

---
+++ dist/amd/can-make-rest.js
@@ -0,0 +1,46 @@
+/*can-make-rest@0.0.1#can-make-rest*/
+define(function (require, exports, module) {
+    var each = require('can-util/js/each');
+    var methodMapping = {
+        item: {
+            'GET': 'getData',
+            'PUT': 'updateData',
+            'DELETE': 'destroyData'
+        },
+        list: {
+            'GET': 'getListData',
+            'POST': 'createData'
+        }
+    };
+    function inferIdProp(url) {
+        var wrappedInBraces = /\{(.*)\}/;
+        var matches = url.match(wrappedInBraces);
+        var isUniqueMatch = matches && matches.length === 2;
+        if (isUniqueMatch) {
+            return matches[1];
+        }
+    }
+    function getItemAndListUrls(url, idProp) {
+        idProp = idProp || inferIdProp(url) || 'id';
+        var itemRegex = new RegExp('\\/\\{' + idProp + '\\}.*');
+        var rootIsItemUrl = itemRegex.test(url);
+        var listUrl = rootIsItemUrl ? url.replace(itemRegex, '') : url;
+        var itemUrl = rootIsItemUrl ? url : url.trim() + '/{' + idProp + '}';
+        return {
+            item: itemUrl,
+            list: listUrl
+        };
+    }
+    module.exports = function (url, idProp) {
+        var data = {};
+        each(getItemAndListUrls(url, idProp), function (url, type) {
+            each(methodMapping[type], function (interfaceMethod, method) {
+                data[interfaceMethod] = {
+                    method: method,
+                    url: url
+                };
+            });
+        });
+        return data;
+    };
+});
